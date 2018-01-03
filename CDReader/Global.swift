6b669a5c3978f08a4cb2edf98861b0b8a4763e24
iosFarmer <nannanYZ@reader.local>
2018-01-03 10:14:38 +0800

    update

---
+++ CDReader/Global.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  CDReader
+//
+//  Created by nannanYZ on 2018/1/2.
+//  Copyright © 2018年 nan. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class var backgound: UIColor {
+        return UIColor.init(r: 242, g: 242, b: 242)
+    }
+    class var theme: UIColor {
+        return UIColor.init(r: 33, g: 99, b: 256)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
